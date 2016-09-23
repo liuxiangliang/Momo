217104ee1e5738a33460161d72a3961875e3bded
lxl <lxl@vip.com>
2016-09-23 09:54:00 +0800

    2.初始化项目

---
+++ Momo/Momo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Momo
+//
+//  Created by kjhd on 16/9/23.
+//  Copyright © 2016年 kjhd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
